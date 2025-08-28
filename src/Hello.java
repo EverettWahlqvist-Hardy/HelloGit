e69d736f1aee8c84ddc558479957937d6e07bcee
Picka <Picka@192.168.1.177>
2025-08-28 15:19:39 -0500

    Add Major

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,19 @@
+package unl.soc;
+
+/*
+ * Author: Everett Wahlqvist-Hardy
+ * 
+ * Date: 2025/08/26
+ * 
+ * A simple hello world program in java
+ * 
+ */
 
 public class Hello {
+	
+	public static void main(String args[]) {
+		System.out.println("Everett Wahlqvist-Hardy!");
+		System.out.println("Computer Science");
+	}
 
 }
